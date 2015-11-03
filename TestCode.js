fa9722104ba8cb3902d0212ab90d20a3efe40ab1
sc2ad <adamznow@gmail.com>
2015-11-03 12:52:49 -0800

    Create TestCode.js, added Base Comment

---
+++ TestCode.js
@@ -0,0 +1,4 @@
+/* 
+Upon Player death, the main character is replaced by a predesessor, but, the surroundings change in a specific way.
+For example, when a player dies in the 1950's, the replacement is in the 1970's, and so forth.
+*/
